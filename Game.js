37192f590a9d4741d27f68f96e6cbadcc017e9b4
Hung Tran <hqt286@vt.edu>
2019-10-27 16:31:33 -0400

    added score page

--- Game.js
+++ Game.js
@@ -6,10 +6,10 @@ var sketchProc = function(processingInstance) {
     //##############################################START HERE##############################################
     //######################################################################################################
     STATE = {
-      OPEN: true,
+      OPEN: false,
       GAME: false,
       INSTRUCTION: false,
-      SCORE: false
+      SCORE: true
     };
 
     var changePage = function changePage(page) {
@@ -59,7 +59,6 @@ var sketchProc = function(processingInstance) {
         imageMode(CENTER);
         rotate(this.angle);
         image(this.img, 0, 0, IMAGESIZE, IMAGESIZE);
-        translate(IMAGESIZE / 2, IMAGESIZE / 2);
         popMatrix();
         this.direction === "cw"
           ? (this.angle -= ROTATESPEED)
@@ -92,13 +91,43 @@ var sketchProc = function(processingInstance) {
     class openObj {
       constructor() {
         this.startShow = false;
-        this.rocketOne = new rocketObj(100, 100, "./images/rocketone.png", "ccw");
-        this.rocketTwo = new rocketObj(350, 750, "./images/rockettwo.png", "ccw");
-        this.rocketThree = new rocketObj(620, 470, "./images/rocketthree.png", "cw");
-        this.rocketFour = new rocketObj(700, 200, "./images/rocketfour.png", "ccw");
-        this.rocketFive = new rocketObj(150, 500, "./images/rocketfive.png", "cw");
-        this.rocketSix = new rocketObj(750, 720, "./images/rocketsix.png", "cw");
-        this.earth = new earthObj(400, 400, "./images/earth.png");        
+        this.rocketOne = new rocketObj(
+          100,
+          100,
+          "./images/rocketone.png",
+          "ccw"
+        );
+        this.rocketTwo = new rocketObj(
+          350,
+          750,
+          "./images/rockettwo.png",
+          "ccw"
+        );
+        this.rocketThree = new rocketObj(
+          620,
+          470,
+          "./images/rocketthree.png",
+          "cw"
+        );
+        this.rocketFour = new rocketObj(
+          700,
+          200,
+          "./images/rocketfour.png",
+          "ccw"
+        );
+        this.rocketFive = new rocketObj(
+          150,
+          500,
+          "./images/rocketfive.png",
+          "cw"
+        );
+        this.rocketSix = new rocketObj(
+          750,
+          720,
+          "./images/rocketsix.png",
+          "cw"
+        );
+        this.earth = new earthObj(400, 400, "./images/earth.png");
       }
 
       display() {
@@ -118,9 +147,106 @@ var sketchProc = function(processingInstance) {
       }
     }
 
+    //############################################### SCORE SCREEN ######################################
 
+    class bigRocketObj {
+      constructor(x, y, img) {
+        this.x = x;
+        this.y = y;
+        this.img = loadImage(img);
+        this.direction = "top";
+        this.angle = 0;
+        this.nextAngle = PI / 2;
+      }
 
-    
+      display() {
+        switch (this.direction) {
+          case "top":
+            if (this.x == 400) {
+              this.angle += ROTATESPEED;
+              if (this.angle > this.nextAngle) {
+                this.direction = "right";
+                this.nextAngle += PI / 2;
+              }
+            } else {
+              this.x++;
+              this.y--;
+            }
+            break;
+
+          case "right":
+            if (this.y == 400) {
+              this.angle += ROTATESPEED;
+              if (this.angle > this.nextAngle) {
+                this.direction = "bottom";
+                this.nextAngle += PI / 2;
+              }
+            } else {
+              this.x++;
+              this.y++;
+            }
+            break;
+
+          case "bottom":
+            if (this.x == 400) {
+              this.angle += ROTATESPEED;
+              if (this.angle > this.nextAngle) {
+                this.direction = "left";
+                this.nextAngle += PI / 2;
+              }
+            } else {
+              this.x--;
+              this.y++;
+            }
+            break;
+
+          case "left":
+            if (this.y == 400) {
+              this.angle += ROTATESPEED;
+              if (this.angle > this.nextAngle) {
+                this.direction = "top";
+                this.nextAngle += PI / 2;
+              }
+            } else {
+              this.x--;
+              this.y--;
+            }
+            break;
+        }
+
+        pushMatrix();
+        imageMode(CENTER);
+        translate(this.x, this.y);
+        rotate(this.angle);
+        image(this.img, 0, 0, IMAGESIZE * 2, IMAGESIZE * 2);
+        popMatrix();
+      }
+    }
+
+    class scoreObj {
+      constructor() {
+        this.bigRocket = new bigRocketObj(70, 400, "./images/rocketone.png");
+        this.score = 0;
+      }
+
+      display(score) {
+        this.score = score;
+        this.bigRocket.display();
+        textAlign(CENTER);
+        textLeading(10); //set line distance
+        textFont(createFont("monospace", 30), 30);
+        text("Achivement", 400, 260);
+        textFont(createFont("monospace", 20), 20);
+        text("Player One", 400, 320);
+        text("Player Two", 400, 380);
+        text("Player Three", 400, 440);
+        textFont(createFont("monospace", 10), 10);
+        text("Press ` to go back", 400, 700);
+      }
+    }
+
+    //############################################### GAME SCREEN ######################################
+
     class playerOjb {}
 
     class gameOjb {
@@ -130,7 +256,7 @@ var sketchProc = function(processingInstance) {
     }
 
     var openScreen = new openObj();
-
+    var scoreScreen = new scoreObj();
 
     var keyPressed = function() {
       if (keyCode === 192) {
@@ -140,10 +266,9 @@ var sketchProc = function(processingInstance) {
 
     var keyReleased = function() {};
 
-    var mouseClicked = function () {
+    var mouseClicked = function() {
       openScreen.startShow = true;
     };
-
 
     var draw = function() {
       background(1, 1, 1);
@@ -152,6 +277,8 @@ var sketchProc = function(processingInstance) {
       } else if (STATE.GAME) {
       } else if (STATE.INSTRUCTION) {
       } else if (STATE.SCORE) {
+        scoreScreen.display(12);
+        openScreen.startShow = false;
       }
     };
 
